de52642e4e59d7bed5a67ccd9bdb1e6b901caa99
Joabe Ferreira Evangelista <joa3.bys@hotmail.com>
2025-01-24 22:55:37 -0300

    feat: add OriginalFizzBuzz

---
+++ src/FizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿OriginalFizzBuzz();
+
+static void OriginalFizzBuzz()
+{
+    const int FIZZ_VALUE = 3;
+    const int BUZZ_VALUE = 5;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        string output = (i % FIZZ_VALUE == 0 ? "Fizz" : "") + 
+                        (i % BUZZ_VALUE == 0 ? "Buzz" : "");
+        
+        Console.WriteLine(!string.IsNullOrEmpty(output) ? output : i);
+    }
+}
+
+
