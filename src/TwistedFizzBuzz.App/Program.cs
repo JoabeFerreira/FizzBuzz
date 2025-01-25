cc8759b0b18eca3cea9433eec0ddbd11576b9776
Joabe Ferreira Evangelista <joa3.bys@hotmail.com>
2025-01-25 01:29:00 -0300

    feat: add TwistedFizzBuzz.App

---
+++ src/TwistedFizzBuzz.App/Program.cs
@@ -0,0 +1,10 @@
+﻿using TwistedFizzBuzz;
+
+var generator = new TwistedFizzBuzzGenerator();
+generator.RegisterDivisorToken(3, "Fizz");
+generator.RegisterDivisorToken(5, "Buzz");
+
+foreach (string fizzBuzz in generator.GenerateFizzBuzzInRange(1, 100))
+{
+    Console.WriteLine(fizzBuzz);
+}
