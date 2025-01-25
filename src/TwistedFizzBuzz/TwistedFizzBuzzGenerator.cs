13239585ab18fd5833acccebc97b0079b60646c8
Joabe Ferreira Evangelista <joa3.bys@hotmail.com>
2025-01-25 01:28:13 -0300

    feat: add TwistedFizzBuzz library

---
+++ src/TwistedFizzBuzz/TwistedFizzBuzzGenerator.cs
@@ -0,0 +1,52 @@
+﻿namespace TwistedFizzBuzz
+{
+    public class TwistedFizzBuzzGenerator
+    {
+        private readonly Dictionary<int, string> tokens = [];
+
+        public void RegisterDivisorToken(int divisor, string token)
+        {
+            tokens[divisor] = token;
+        }
+
+        public IEnumerable<string> GenerateFizzBuzzInRange(int start, int end)
+        {
+            var range = GenerateRange(start, end);
+            return GenerateFizzBuzz(range);
+        }
+
+        private static int[] GenerateRange(int start, int end)
+        {
+            if (start <= end)
+            {
+                return Enumerable.Range(start, end - start + 1).ToArray();
+            }
+            else
+            {
+                return Enumerable.Range(end, start - end + 1).Reverse().ToArray();
+            }
+        }
+
+        public IEnumerable<string> GenerateFizzBuzz(params int[] values)
+        {
+            foreach (int value in values)
+            {
+                yield return GetFizzBuzzOutput(value);
+            }
+        }
+
+        private string GetFizzBuzzOutput(int value)
+        {
+            string output = "";
+            foreach (KeyValuePair<int, string> token in tokens)
+            {
+                if (value % token.Key == 0)
+                {
+                    output += token.Value;
+                }
+            }
+
+            return !string.IsNullOrEmpty(output) ? output : value.ToString();
+        }
+    }
+}
